7e36c9e657b57f8746064f2624b0ce44105d30af
Rafael Schreiber <rafael@backend.works>
2020-09-28 18:41:27 +0200

    modified aba to use charout

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -3,15 +3,14 @@
 #include <thread>
 #include <csignal>
 #include <unistd.h>
+#include <cerrno>
 
 int main(){
     pid_t pid{fork()};
     if (pid == 0){
-        while(1){
-            std::cout << "A" << std::flush;
-            std::chrono::milliseconds sleeptime(500);
-            std::this_thread::sleep_for(sleeptime);
-        }
+        execl("./charout", "charout", "A", nullptr);
+        std::cerr << strerror(errno) << std::endl;
+        exit(1);
     } else {
         int counter{0};
         while(counter < 6){

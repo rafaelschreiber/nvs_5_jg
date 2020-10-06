8709fc7e9fa76d6defddc5b017c4d80eeb151284
Rafael Schreiber <rafael@backend.works>
2020-10-06 13:49:50 +0200

    finished 02_threads :)

--- 02_threads/src/contest.cpp
+++ 02_threads/src/contest.cpp
@@ -16,19 +16,21 @@ using namespace std;
 
 class Car{
     private:
-        string car_name; 
+        string car_name;
+        int laps{0};
         double race_time{0.0};
     public:
         static double random_sleep();
-        Car(string name);
+        Car(string name, int laps);
         ~Car();
         void operator()();
         string get_car_name();
         double get_race_time();
 };
 
-Car::Car(string name){
+Car::Car(string name, int laps){
     car_name = name;
+    this->laps = laps;
 }
 
 string Car::get_car_name(){
@@ -42,7 +44,7 @@ double Car::get_race_time(){
 double Car::random_sleep(){
     random_device rd;
     mt19937 gen{rd()};
-    uniform_real_distribution<> dis{0, 1};
+    uniform_real_distribution<> dis{1, 10};
     double sleep_time{dis(gen)};
     this_thread::sleep_for(chrono::milliseconds{u_int (sleep_time * 1000)});
     return sleep_time;
@@ -52,7 +54,7 @@ void Car::operator()(){
     int counter{0};
     string startingracemsg = {car_name + " starting race...\n"};
     cout << startingracemsg;
-    while(counter < 10){
+    while(counter < laps){
         counter++;
         double lap_time = random_sleep();
         race_time += lap_time;
@@ -69,12 +71,13 @@ Car::~Car(){
     return;
 }
 
-void fiat_punto(double& race_time){
+
+void fiat_punto(double& race_time, int laps){
     string car_name{"Fiat Punto"};
     int counter{0};
     string startingracemsg = {car_name + " starting race...\n"};
     cout << startingracemsg;
-    while(counter < 10){
+    while(counter < laps){
         counter++;
         double lap_time = Car::random_sleep();
         race_time += lap_time;
@@ -87,12 +90,13 @@ void fiat_punto(double& race_time){
     }
 }
 
-int main() {
-    Car corsa("Opel Corsa");
-    thread t1{ref(corsa)};
+
+void race(int laps){
+    Car corsa("Opel Corsa", laps);
+    thread t1{&Car::operator(), ref(corsa)};
 
     double total_time_punto{0.0};
-    thread t2(fiat_punto, ref(total_time_punto));
+    thread t2(fiat_punto, ref(total_time_punto), laps);
 
     t1.join();
     t2.join();
@@ -130,6 +134,48 @@ int main() {
         buf.str("");
         cout << totalinfo_punto;
     }
+}
 
-    return 0;
+void help(string command){
+    cout << "Usage: " << command << " [-h | --help | LAPS]\n";
+}
+
+void error(string argument){
+    cout << "Could not convert: " << argument << endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "No arguments given.\nRun with --help for more information.\n";
+        return 1; 
+    }
+    if (string(argv[1]) == "-h" || string(argv[1]) == "--help"){
+        help(argv[0]);
+        return 0;
+    }
+    try {
+        size_t pos = 0;
+        stoi(string(argv[1]), &pos);
+        if (pos != string(argv[1]).length()){
+            error(string(argv[1]));
+            cout << "Run with --help for more information." << endl;
+            return 1; 
+        } else if (stoi(string(argv[1])) >= 1 && stoi(string(argv[1])) < 16){
+            cout << "Beginning race with " << string(argv[1]) << " laps...\n";
+            race(stoi(string(argv[1])));
+            return 0;
+        } else if (stoi(string(argv[1])) < 1 || stoi(string(argv[1])) >= 16){
+            cout << "Out of range (1 <= LAPS < 16): " << string(argv[1]) << endl;
+            cout << "Run with --help for more information." << endl;
+            return 1;
+        } else {
+            error(string(argv[1]));
+            cout << "Run with --help for more information." << endl;
+            return 1;
+        }
+    } catch (...){
+        error(string(argv[1]));
+        cout << "Run with --help for more information." << endl;
+        return 1;
+    }
 }

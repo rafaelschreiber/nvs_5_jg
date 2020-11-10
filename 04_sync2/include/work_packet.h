6511dbb8280736b95e6517d2f1f20138c8cc470f
Rafael Schreiber <rafael@backend.works>
2020-11-10 11:17:21 +0100

    add work packet class

--- 04_sync2/include/work_packet.h
+++ 04_sync2/include/work_packet.h
@@ -5,3 +5,16 @@
  * Date: 10-11-2020
  */
 
+#pragma once
+
+class WorkPacket{
+    private:
+        int work_id;
+    public:
+        WorkPacket(int work_id){
+            this->work_id = work_id;
+        }
+        int get_work_id() const {
+            return this->work_id
+        }
+};

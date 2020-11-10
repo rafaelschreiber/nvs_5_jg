a379a8a0ddf378aa17362ee121823b04557d4a6f
Rafael Schreiber <rafael@backend.works>
2020-11-10 13:29:54 +0100

    implement random sleep for boss and workers

--- 04_sync2/src/loadsim.cpp
+++ 04_sync2/src/loadsim.cpp
@@ -9,25 +9,34 @@
 #include <thread>
 #include <chrono>
 #include <sstream>
+#include <random>
+#include <iomanip>
 #include "work_queue.h"
 #include "work_packet.h"
 
 using namespace std;
 
 void worker(int id, WorkQueue& q){
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{1, 10};
+    double sleep_time{};
+    
     ostringstream buf;
     while(true){
+        sleep_time = dis(gen);
         buf << "W" << id << ": Want work packet" << endl;
         cout << buf.str() << flush;
         buf.str("");
         WorkPacket current_packet = q.pop();
         buf << "W" << id << ": Got work packet " 
-             << current_packet.get_work_id() << endl;
+            << current_packet.get_work_id() << endl;
         cout << buf.str() << flush;
         buf.str("");
-        this_thread::sleep_for(chrono::milliseconds(1000));
+        this_thread::sleep_for(chrono::milliseconds(u_int (sleep_time * 1000)));
         buf << "W" << id << ": Processed work packet "
-             << current_packet.get_work_id() << endl;
+            << current_packet.get_work_id() << setprecision(2)  
+            << " (" << sleep_time << "s)" << endl;
         cout << buf.str() << flush;
         buf.str("");
     }
@@ -35,17 +44,32 @@ void worker(int id, WorkQueue& q){
 
 int main() {
     WorkQueue wq{};
-    for (int counter{0}; counter <= 10; counter++){
-        wq.push(WorkPacket{counter});
-        cout << "B: Submitted work packet " << counter << endl;
-        this_thread::sleep_for(chrono::milliseconds(500));
-    }
-
     thread worker1{worker, 1, ref(wq)};
     thread worker2{worker, 2, ref(wq)};
+    thread worker3{worker, 3, ref(wq)};
+
+    ostringstream buf;
+    random_device rd;
+    mt19937 gen{rd()};
+    uniform_real_distribution<> dis{0, 1};
+    double sleep_time{};
+
+    for (int counter{0}; counter <= 10; counter++){
+        sleep_time = dis(gen);
+        buf << "B:  Waiting to submit work packet " << counter << endl;
+        cout << buf.str() << flush;
+        buf.str("");
+        this_thread::sleep_for(chrono::milliseconds(u_int (sleep_time * 1000)));
+        wq.push(WorkPacket{counter});
+        buf << "B:  Submitted work packet " << counter << setprecision(2) 
+            << " (" << sleep_time << "s)" << endl;
+        cout << buf.str() << flush;
+        buf.str("");
+    }
 
     worker1.join();
     worker2.join();
+    worker3.join();
 
     return EXIT_SUCCESS;
 }

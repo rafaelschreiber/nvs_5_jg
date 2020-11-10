5478d7465ceb532238356b5046dfcb302c5183b9
Rafael Schreiber <rafael@backend.works>
2020-11-10 11:04:45 +0100

    add project structure 04_sync2

---
+++ 04_sync2/include/work_packet.h
@@ -0,0 +1,7 @@
+/*
+ * Author: Rafael Schreiber (i16066)
+ * Project: 04_sync
+ * File: work_packet.h
+ * Date: 10-11-2020
+ */
+
---
+++ 04_sync2/src/loadsim.cpp
@@ -0,0 +1,10 @@
+/*
+ * Author: Rafael Schreiber (i16066)
+ * Project: 04_sync
+ * File: loadsim.cpp
+ * Date: 10-11-2020
+ */
+
+int main() {
+    return 0;
+}

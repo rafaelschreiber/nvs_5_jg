3e8e16c484e686aa4a8acc69f43586a6e269a063
Rafael Schreiber <rafael@backend.works>
2020-12-29 17:28:08 +0100

    finish cli argument passing

--- 06_future/src/main.cpp
+++ 06_future/src/main.cpp
@@ -5,6 +5,32 @@
  * Date: 07-12-2020
  */
 
-int main() {
+#include <iostream>
+#include <vector>
+#include "CLI11.hpp"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    vector<string> large_numbers;
+    bool async{false};
+
+    CLI::App app("Factor numbers");
+    app.add_option("number", large_numbers, "numbers to factor") -> required() 
+    ->  check([](const std::string &str) {
+            auto is_valid = str.find_first_not_of("0123456789");
+            if(is_valid == string::npos){
+                return std::string();
+            } else {
+                return std::string(str + " contains not numeric character");
+            }
+        });
+    app.add_flag("-a, --async", async, "async");
+    CLI11_PARSE(app, argc, argv);
+
+    for(auto elem : large_numbers){
+        cout << elem << endl;
+    }
+    cout << "Async: " << async << endl;
     return 0;
 }

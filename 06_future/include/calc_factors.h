02bc26cc24906318f1e2f4e23ef3b5290aea7c85
Rafael Schreiber <rafael@backend.works>
2020-12-29 18:10:55 +0100

    implement prime factoring

--- 06_future/include/calc_factors.h
+++ 06_future/include/calc_factors.h
@@ -4,8 +4,9 @@
 // ko
 // adapted from M. Gallant  1/98
 
-#include <vector>
+#pragma GCC diagnostic ignored "-Wempty-body"
 
+#include <vector>
 #include "InfInt.h"
 
 inline std::vector<InfInt> get_factors(InfInt number) { // number should be positive
--- 06_future/src/main.cpp
+++ 06_future/src/main.cpp
@@ -8,15 +8,26 @@
 #include <iostream>
 #include <vector>
 #include "CLI11.hpp"
+#include "calc_factors.h"
+#include "InfInt.h"
 
 using namespace std;
 
+vector<InfInt> convert_to_infint_vector(vector<string> s_vector){
+    vector<InfInt> infint_vector;
+    for(auto s_number : s_vector){
+        InfInt infint_number = s_number;
+        infint_vector.push_back(infint_number);
+    }
+    return infint_vector;
+}
+
 int main(int argc, char** argv) {
-    vector<string> large_numbers;
+    vector<string> factor_numbers;
     bool async{false};
 
     CLI::App app("Factor numbers");
-    app.add_option("number", large_numbers, "numbers to factor") -> required() 
+    app.add_option("number", factor_numbers, "numbers to factor") -> required() 
     ->  check([](const std::string &str) {
             auto is_valid = str.find_first_not_of("0123456789");
             if(is_valid == string::npos){
@@ -28,8 +39,15 @@ int main(int argc, char** argv) {
     app.add_flag("-a, --async", async, "async");
     CLI11_PARSE(app, argc, argv);
 
-    for(auto elem : large_numbers){
-        cout << elem << endl;
+    vector<InfInt> infint_factor_numbers = convert_to_infint_vector(factor_numbers);
+
+    for(auto elem : infint_factor_numbers){
+        cout << elem << ": ";
+        auto elem_factors = get_factors(elem);
+        for (auto factor : elem_factors){
+            cout << factor << " ";
+        }
+        cout << endl;
     }
     cout << "Async: " << async << endl;
     return 0;

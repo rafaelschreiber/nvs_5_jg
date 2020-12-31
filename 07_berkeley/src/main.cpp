9185a4d517a8b14e9701c53e572d2c07ce586ab0
Rafael Schreiber <rafael@backend.works>
2020-12-31 19:02:47 +0100

    implement methods to get and set channels and conntect them

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -29,6 +29,7 @@ class Channel{
 
 class TimeSlave{
     Clock machine_clock;
+    Channel* slave_channel_ptr = new Channel();
 
     public:
         TimeSlave(string machine_name, int hours, int minutes, int seconds){
@@ -36,10 +37,16 @@ class TimeSlave{
         }
 
         void operator()(){ }
+
+        Channel* get_channel(){
+            return slave_channel_ptr;
+        }
 };
 
 class TimeMaster{
     Clock machine_clock;
+    Channel* channel1;
+    Channel* channel2;
 
     public:
         TimeMaster(string machine_name, int hours, int minutes, int seconds){
@@ -47,16 +54,32 @@ class TimeMaster{
         }
 
         void operator()(){ }
+
+        void set_channel1(Channel* channel){
+            channel1 = channel;
+        }
+
+        void set_channel2(Channel* channel){
+            channel2 = channel;
+        }        
 };
 
 int main() {
-    thread master{TimeMaster("master", 9, 41, 0)};
-    thread slave1{TimeSlave("slave1", 9, 41, 0)};
-    thread slave2{TimeSlave("slave2", 9, 41, 0)};
+    TimeMaster master("master", 9,41, 0);
+
+    TimeSlave slave1("slave1", 12, 0, 0);
+    TimeSlave slave2("slave2", 0, 0, 0);
+
+    master.set_channel1(slave1.get_channel());
+    master.set_channel2(slave2.get_channel());
     
-    master.join();
-    slave1.join();
-    slave2.join();
+    thread master_thread{master};
+    thread slave1_thread{slave1};
+    thread slave2_thread{slave2};
+
+    master_thread.join()
+    slave1_thread.join()
+    slave2_thread.join()
 
     return 0;
 }
